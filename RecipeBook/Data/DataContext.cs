3a49f80186610aa2f5729fabbf2c59c60e7c8772
Christos Marroquin <christos.marroquin@hotmail.com>
2024-10-14 19:00:03 -0600

    FEATURE: Add DataContext and relations

---
+++ RecipeBook/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeBook.Models;
+using System.Reflection.Metadata;
+
+namespace RecipeBook.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Chef> Chefs { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Chef>()
+                .HasMany(e => e.Recipes)
+                .WithOne(e => e.Chef)
+                .HasForeignKey(e => e.ChefId)
+                .IsRequired();
+        }
+
+    }
+}
