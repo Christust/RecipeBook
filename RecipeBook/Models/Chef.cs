db9c28b7460f0b2c9bcc436f1eb3205cb08dd27a
Christos Marroquin <christos.marroquin@hotmail.com>
2024-10-14 18:59:41 -0600

    FEATURE: Add models Chef and Recipe

---
+++ RecipeBook/Models/Chef.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RecipeBook.Models
+{
+    public class Chef
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        [JsonIgnore]
+        public ICollection<Recipe> Recipes { get; } = new List<Recipe>();
+    }
+}
---
+++ RecipeBook/Models/Recipe.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace RecipeBook.Models
+{
+    public class Recipe
+    {
+        public long Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public int ChefId { get; set; }
+        [JsonIgnore]
+        public Chef Chef { get; set; } = null!;
+
+    }
+}
